e9102405056606422e1923e546a3a89fd5e5cde5
FELIPE SIQUEIRA DOS REIS <88809590+FelipeSking@users.noreply.github.com>
2024-09-13 00:47:19 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages 
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="processimagedrm",
+    version="0.0.1",
+    author="FelipeSking",
+    author_email="felipesiqueirareis@gmail.com",
+    description="Process Images",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="my_github_repository_project_link",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+
+)
